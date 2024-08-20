f5b66b32b30eac58be256664c4a41512b214e93e
Keith Carlos <keithcarlos34@gmail.com>
2024-08-19 22:01:46 -0400

    updates

--- src/components/Verify.tsx
+++ src/components/Verify.tsx
@@ -65,9 +65,12 @@ const Verify = ({ data, token }: { data: any, token: string }) => {
                     (
                         <div className="tw-h-[100dvh] tw-flex tw-justify-center tw-items-center tw-flex-col tw-gap-5 tw-bg-red-400">
                             <h1 className="tw-text-white tw-font-extrabold tw-text-[90px]">The Forum</h1>
-                            <div className="tw-cursor-pointer tw-rounded-md tw-bg-black tw-py-[16px] tw-px-[16px] tw-border-[5px] tw-border-black hover:tw-brightness-90">
+                            <div className="tw-rounded-md tw-bg-black tw-py-[16px] tw-px-[16px] tw-border-[5px] tw-border-black">
                                 <span className="tw-font-bold tw-text-[24px] tw-text-red-600">{data.error}</span>
                             </div>
+                            <Link href="/" className="tw-underline tw-font-bold tw-text-red-700 tw-brightness-90" onClick={e => setIsVisible(true)}>
+                                Log In
+                            </Link>
                         </div>
                     )
             )
