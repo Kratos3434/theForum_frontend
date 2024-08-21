fc243c8f94386782396b34554936d0ffb40d9edc
Keith Carlos <keithcarlos34@gmail.com>
2024-08-21 17:05:59 -0400

    updates

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -7,6 +7,8 @@ import User from "@/model/User";
 import SignupController from "@/controller/SignupController";
 import CloseIcon from '@mui/icons-material/Close';
 import MailIcon from '@mui/icons-material/Mail';
+import Link from "next/link";
+import Loading from "./Loading";
 
 const Signup = () => {
     const setIsSignupVisible = useSetAtom(isSignupVisibleAtom);
@@ -14,9 +16,10 @@ const Signup = () => {
     const [error, setError] = useState("");
     const user = useState(new User())[0];
     const [open, isOpen] = useState(false);
+    const [loading, isLoading] = useState(false);
 
     const handleSignup = (e: FormEvent<HTMLFormElement>) => {
-        
+
         e.preventDefault();
         const { status, error: err } = SignupController.signup(user);
 
@@ -29,54 +32,55 @@ const Signup = () => {
     }
 
     return (
-        <div className={`tw-fixed tw-top-0 tw-left-0 tw-bg-[rgb(0,0,0)] tw-bg-[rgba(0,0,0,0.4)] tw-w-full tw-h-full`}>
-            <div className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center"
-                onClick={e => setIsSignupVisible(false)}>
-                <div className="tw-bg-white tw-shadow-lg tw-rounded-md tw-py-[8px] tw-px-[16px] tw-max-w-[500px] tw-w-full tw-relative"
-                    onClick={e => e.stopPropagation()}>
-                    <div className="tw-flex tw-justify-between">
-                        <small className="tw-text-blue-600 tw-font-bold tw-cursor-pointer"
-                            onClick={e => {
-                                setIsSignupVisible(false);
-                                setIsVisible(true);
-                            }}>
-                            Log In
-                        </small>
-                        <small className="tw-px-[8px] tw-bg-gray-300 tw-rounded-full hover:tw-brightness-90 tw-cursor-pointer"
-                            onClick={e => setIsSignupVisible(false)}>
-                            X
-                        </small>
-                    </div>
-                    <div className="tw-text-[25px] tw-font-bold tw-my-3">
-                        Sign Up <small className="tw-text-[15px] tw-font-normal tw-text-red-400">It&apos;s quick and easy</small>
-                    </div>
-                    <form className="tw-flex tw-flex-col tw-gap-5" onSubmit={handleSignup}>
-                        <div className="tw-flex tw-flex-col tw-gap-2">
-                            <Input type="email" placeholder="Email" className="tw-py-[16px] tw-px-[16px]" 
-                            onChange={e => user.setEmail(e.target.value)} />
-                            <Input type="text" placeholder="Username" className="tw-py-[16px] tw-px-[16px]" 
-                            onChange={e => user.setEmail(e.target.value)} />
-                            <Input type="password" placeholder="Password" className="tw-py-[16px] tw-px-[16px]" 
-                            onChange={e => user.setPassword(e.target.value)} />
-                            <Input type="password" placeholder="Confirm Password" className="tw-py-[16px] tw-px-[16px]" 
-                            onChange={e => user.setPassword2(e.target.value)} />
+        <div className="tw-w-full tw-h-[100dvh] tw-flex tw-justify-center tw-items-center tw-bg-red-400">
+            <div>
+                <span className="tw-text-red-600 tw-font-extrabold tw-text-[50px]">
+                    The Forum
+                </span>
+                <div className="tw-my-[20px]">
+                    <div>
+                        <div className="tw-text-white tw-font-bold tw-text-[30px]">
+                            Register
                         </div>
-                        <div className="tw-flex tw-flex-col tw-gap-2 tw-items-center">
+                        <small className="tw-text-white">
+                            Already have an account? &nbsp;
+                            <Link href="/login" className="tw-text-blue-600 hover:tw-underline">
+                                Log in
+                            </Link>
+                        </small>
+                    </div>
+                    <form className="tw-my-[15px] tw-flex tw-flex-col tw-gap-5" onSubmit={handleSignup}>
+                        <div>
+                            <label className="tw-text-white">
+                                Email or Username
+                            </label>
+                            <Input className="tw-px-[16px] tw-py-[8px] tw-my-[5px] tw-border-black tw-bg-red-300" type="text" placeholder=""
+                            />
+                        </div>
+                        <div>
+                            <label className="tw-text-white">
+                                Password
+                            </label>
+                            <Input className="tw-px-[16px] tw-py-[8px] tw-my-[5px] tw-border-black tw-bg-red-300" type="password" placeholder=""
+                            />
+                        </div>
+                        <div className="tw-mt-[30px] tw-text-center">
                             {
                                 error &&
                                 (
-                                    <small className="tw-text-red-600">
+                                    <small className="tw-text-black tw-font-extrabold">
                                         *{error}
                                     </small>
                                 )
                             }
-                            <button className="tw-rounded-md tw-bg-red-500 tw-font-bold tw-py-[8px] tw-px-[16px] hover:tw-brightness-90">
-                                Sign Up
+                            <button className="tw-w-full tw-bg-white tw-py-[8px] tw-rounded-xl tw-font-bold hover:tw-brightness-95">
+                                Register
                             </button>
                         </div>
                     </form>
                 </div>
             </div>
+            {loading && <Loading message="Logging in, please wait..." />}
             {
                 open &&
                 (
