586f80d66a5635a4ce77abd2d9a7db01d0c1f391
Keith Carlos <keithcarlos34@gmail.com>
2024-08-24 14:59:12 -0400

    updates

--- src/model/Post.ts
+++ src/model/Post.ts
@@ -3,7 +3,7 @@ import User from "./User";
 class Post {
     private id: number;
     private title: string;
-    private descripotion?: string;
+    private description?: string;
     private media?: string;
     private author: User;
     private createdAt: string;
@@ -12,10 +12,68 @@ class Post {
     constructor(id?:number, title?:string, description?: string, media?: string, author?: User, createdAt?: string, updatedAt?: string) {
         this.id = id ?? 0;
         this.title = title ?? "";
-        this.descripotion = description;
+        this.description = description;
         this.media = media;
         this.author = author ?? new User();
         this.createdAt = createdAt ?? "";
         this.updatedAt = updatedAt;
     }
+
+    public getId() {
+        return this.id;
+    }
+
+    public getTitle() {
+        return this.title;
+    }
+
+    public getDescription() {
+        return this.description;
+    }
+
+    public getMedia() {
+        return this.media;
+    }
+
+    public getAuthor() {
+        return this.author;
+    }
+
+    public getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public setId(id: number) {
+        this.id = id;
+    }
+
+    public setTitle(title: string) {
+        this.title = title;
+    }
+
+    public setDescription(description: string) {
+        this.description = description;
+    }
+
+    public setMedia(media: string) {
+        this.media = media;
+    }
+
+    public setAuthor(author: User) {
+        this.author = author;
+    }
+
+    public setCreatedAt(createdAt: string) {
+        this.createdAt = createdAt;
+    }
+
+    public setUpdatedAt(updatedAt?: string) {
+        this.updatedAt = updatedAt;
+    }
 }
+
+export default Post;
